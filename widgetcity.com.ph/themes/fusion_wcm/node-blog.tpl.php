3fad3a0a795a208b67e3fe9fe41a6853f5992248
Mark Jayson Nicolas <dzieyzone@Marks-MacBook-Pro.local>
2012-08-10 02:03:19 +0800

    changes on mac

--- widgetcity.com.ph/themes/fusion_wcm/node-blog.tpl.php
+++ widgetcity.com.ph/themes/fusion_wcm/node-blog.tpl.php
@@ -38,7 +38,7 @@
   <div id="node-bottom" class="node-bottom row nested">
     <div id="node-bottom-inner" class="node-bottom-inner inner">
       <?php print $node_bottom; ?>
-    </div><!-- /node-bottom-inner -->
-  </div><!-- /node-bottom -->
+    </div>
+  </div>
   <?php endif; ?>
-</div><!-- /node-<?php print $node->nid; ?> -->
+</div>
