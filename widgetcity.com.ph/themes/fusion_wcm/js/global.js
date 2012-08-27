9909025d6bdddbfd1a2683f1148922d877c776fb
Mark Jayson Nicolas <dzieyzone@Marks-MacBook-Pro.local>
2012-08-27 17:04:34 +0800

    update theme

--- widgetcity.com.ph/themes/fusion_wcm/js/global.js
+++ widgetcity.com.ph/themes/fusion_wcm/js/global.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
-  $("dl.dropdown .menu a").click(function() {
-		$("dd ul", $(this).parents('.dropdown')).toggle();
-		return false;
-	});
-/*
-	$("dd.dropdown dd ul li a").click(function() {
-		$("dd.dropdown dd ul").hide();
-	});
-*/							
-	$(document).bind('click', function(e) {
-		var $clicked = $(e.target);
-		if (! $clicked.parents().hasClass("dropdown"))
-				$("dd ul").hide();
-	});
+  var top = $('#sidebar-first-inner').offset().top - parseFloat($('#sidebar-first-inner').css('marginTop').replace(/auto/, 0));
+  var footTop = $('#footer-wrapper').offset().top - parseFloat($('#footer-wrapper').css('marginTop').replace(/auto/, 0));
+
+  var maxY = footTop - $('#sidebar-first-inner').outerHeight();
+
+  $(window).scroll(function(evt) {
+    var y = $(this).scrollTop();
+    if (y > top) {
+      if (y < maxY) {
+        $('#sidebar-first-inner').addClass('fixed').removeAttr('style');
+      }
+      else {
+        $('#sidebar-first-inner').removeClass('fixed').css({
+          position: 'absolute',
+          top: (maxY - top) + 'px'
+        });
+      }
+    }
+    else {
+      $('#sidebar-first-inner').removeClass('fixed');
+    }
+  });
 });
